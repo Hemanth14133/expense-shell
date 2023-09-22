c3460b77e98a928aaa0c2f54ac4e058309a4ce05
Hemanth14133 <hemanth14133@gmail.com>
2023-09-22 15:50:19 +0530

    variables & functions

--- backend.sh
+++ backend.sh
@@ -3,33 +3,43 @@ component=backend
 
 echo Install NodeJs Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>>$log_file
+echo $?
 
 echo Install NodeJs
 dnf install nodejs -y  &>>$log_file
+echo $?
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service  &>>$log_file
+echo $?
 
 echo Add Application User
 useradd expense  &>>$log_file
+echo $?
 
 echo clean App Content
 rm -rf /app   &>>$log_file
 mkdir /app
 cd /app
+echo $?
 
 download_and_extract
+echo $?
 
 echo Download Dependencies
 npm install  &>>$log_file
+echo $?
 
 echo Start Backend Service
 systemctl daemon-reload  &>>$log_file
 systemctl enable backend  &>>$log_file
 systemctl restart backend  &>>$log_file
+echo $?
 
 echo Install MYSQL Client
 dnf install mysql -y  &>>$log_file
+echo $?
 
 echo Load Schema
 mysql -h mysql.hemanth14133.online -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$log_file
+echo $?
--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,25 @@
 source common.sh
 component=frontend
+
 echo Installing Nginx
 dnf install nginx -y   &>>$log_file
+echo $?
 
 echo Placing Expense Config File in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf  &>>$log_file
+echo $?
 
 echo Removing Old Nginx Content
 rm -rf /usr/share/nginx/html/*  &>>$log_file
+echo $?
 
 cd /usr/share/nginx/html
+echo $?
 
 download_and_extract
+echo $?
 
 echo starting Nginx Service
 systemctl enable nginx   &>>$log_file
 systemctl restart nginx  &>>$log_file
+echo $?
