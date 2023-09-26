864719de56e6cb7a946f43abb59ff063241132b9
Hemanth14133 <hemanth14133@gmail.com>
2023-09-26 12:17:00 +0530

    variables & functions

--- backend.sh
+++ backend.sh
@@ -3,84 +3,42 @@ component=backend
 
 echo Install NodeJs Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Install NodeJs
 dnf install nodejs -y  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo Add Application User
 useradd expense  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo clean App Content
 rm -rf /app   &>>$log_file
 mkdir /app
 cd /app
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 download_and_extract
 
 echo Download Dependencies
 npm install  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILED
-  exit 1
-fi
+stat_check
 
 echo Start Backend Service
 systemctl daemon-reload  &>>$log_file
 systemctl enable backend  &>>$log_file
 systemctl restart backend  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILED
-fi
+stat_check
 
 echo Install MYSQL Client
 dnf install mysql -y  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILED
-fi
+stat_check
 
 echo Load Schema
 mysql -h mysql.hemanth14133.online -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILED
-fi
+stat_check
--- common.sh
+++ common.sh
@@ -5,18 +5,19 @@ download_and_extract()
 
    echo Download $component code
    curl  -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
-   if [ $? -eq 0 ]; then
-  echo -e"\e[32mSUCCESS\e[0m"
-else
-  echo -e"\e[31mFAILED\e[0m"
-fi
+   echo $?
 
    echo Extrating $component Code
    unzip /tmp/$component.zip   &>>$log_file
-   if [ $? -eq 0 ]; then
-  echo -e"\e[32mSUCCESS\e[0m"
-else
-  echo -e"\e[31mFAILED\e[0m"
-fi
+   echo $?
 
+}
+
+stat_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+  fi
 }
