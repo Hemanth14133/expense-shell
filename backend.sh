8f624e2640e9ee5d3b603e60a087873d19bd3198
Hemanth14133 <hemanth14133@gmail.com>
2023-09-26 10:55:01 +0530

    variables & functions

--- backend.sh
+++ backend.sh
@@ -3,42 +3,78 @@ component=backend
 
 echo Install NodeJs Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Install NodeJs
 dnf install nodejs -y  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Add Application User
 useradd expense  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo clean App Content
 rm -rf /app   &>>$log_file
 mkdir /app
 cd /app
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 download_and_extract
 
 echo Download Dependencies
 npm install  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Start Backend Service
 systemctl daemon-reload  &>>$log_file
 systemctl enable backend  &>>$log_file
 systemctl restart backend  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Install MYSQL Client
 dnf install mysql -y  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Load Schema
 mysql -h mysql.hemanth14133.online -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
