df784d0647024262b6e4c8bc4dbc76e1a2d083aa
Hemanth14133 <hemanth14133@gmail.com>
2023-09-26 11:41:49 +0530

    variables & functions

--- common.sh
+++ common.sh
@@ -5,10 +5,18 @@ download_and_extract()
 
    echo Download $component code
    curl  -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip  &>>$log_file
-   echo $?
+   if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
    echo Extrating $component Code
    unzip /tmp/$component.zip   &>>$log_file
-   echo $?
+   if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 }
--- frontend.sh
+++ frontend.sh
@@ -3,15 +3,27 @@ component=frontend
 
 echo Installing Nginx
 dnf install nginx -y   &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Placing Expense Config File in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Removing Old Nginx Content
 rm -rf /usr/share/nginx/html/*  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 cd /usr/share/nginx/html
 
@@ -20,4 +32,8 @@ download_and_extract
 echo starting Nginx Service
 systemctl enable nginx   &>>$log_file
 systemctl restart nginx  &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
